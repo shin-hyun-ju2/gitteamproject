b6b446c937bc0aff6663ab57c7ebf36f7b7a51e2
zooH <102849251+jellyzoo@users.noreply.github.com>
2025-09-01 14:12:46 +0900

    장바구니 기능 추가
    장바구니 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import './App.css';
 import Install from './install';
 import StarRating from './components/StarRating';
 
+import { Routes, Route } from 'react-router-dom';
+
+import Cart from './cart/Cart';
+
+
 function App() {
   return (
     <div className="App">
-      <h1>PWA 테스트</h1>
-      <Install />
-      <StarRating itemId={1} />
-      <StarRating itemId={2} />
-      <StarRating itemId={3} />
+      <Routes>
+        {/* 각 경로에 맞는 컴포넌트를 렌더링 */}
+        <Route path="/" element={
+          <>
+            <h1>PWA 테스트</h1>
+            <Install />
+            <StarRating itemId={1} />
+            <StarRating itemId={2} />
+            <StarRating itemId={3} />
+          </>
+        } />
+        <Route path="/Cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
